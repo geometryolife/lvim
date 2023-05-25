868df05af6b1056d8961b75258c9b18d9f7eb4cd
Joe Chen <geometryolife@gmail.com>
2023-05-25 14:54:55 +0800

    add mapping M-x as : for normal and visual mode

--- config.lua
+++ config.lua
@@ -2,3 +2,7 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+
+-- Normal and Visual mode mapping M-x as :
+lvim.keys.normal_mode["<M-x>"] = ":<C-u>call feedkeys(':')<CR>"
+lvim.keys.visual_mode["<M-x>"] = ":<C-u>call feedkeys(':')<CR>gv"
