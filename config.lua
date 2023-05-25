76550e2b39528acd3f60b8fbea1c98dabdf5f533
Joe Chen <geometryolife@gmail.com>
2023-05-26 00:01:23 +0800

    add H/J/K/L to move fast and <++> placehoder magic

--- config.lua
+++ config.lua
@@ -17,6 +17,16 @@ lvim.keys.visual_mode["<M-x>"] = ":<C-u>call feedkeys(':')<CR>gv"
 -- Mapping SPC-k as K
 lvim.keys.normal_mode['<Leader>k'] = 'K'
 
+-- Move fast
+lvim.keys.normal_mode['J'] = "5j"
+lvim.keys.normal_mode['K'] = "5k"
+lvim.keys.normal_mode['H'] = "0"
+lvim.keys.normal_mode['L'] = "$"
+lvim.keys.visual_mode['J'] = "5j"
+lvim.keys.visual_mode['K'] = "5k"
+lvim.keys.visual_mode['H'] = "0"
+lvim.keys.visual_mode['L'] = "$"
+lvim.keys.normal_mode['<Leader><Leader>'] = '<Esc>/<++><CR><Cmd>nohlsearch<CR>"_c4l'
 
 -- LSP
 lvim.lsp.buffer_mappings.normal_mode['<Leader>k'] = { vim.lsp.buf.hover, "Show hover" }
